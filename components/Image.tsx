b7fa04e5f413cdf49373a0d653208760fba17c78
clark <15702455610@163.com>
2025-08-09 12:36:21 +0800

    feat: new code

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,9 +3,7 @@ import NextImage, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH_STATIC
 
 const Image = ({ src, ...rest }: ImageProps) => {
-
   return <NextImage src={`${basePath || ''}${src}`} unoptimized={true} {...rest} />
-
 }
 
 export default Image
