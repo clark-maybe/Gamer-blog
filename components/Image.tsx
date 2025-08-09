f2954c88ad52f8b0fa0c290b80f87fa6a0ef64ac
clark <15702455610@163.com>
2025-08-09 12:33:46 +0800

    feat: new code

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,10 +3,8 @@ import NextImage, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH_STATIC
 
 const Image = ({ src, ...rest }: ImageProps) => {
-  const isExternal = typeof src === 'string' && src.startsWith('http')
-  const imageSrc = isExternal ? src : `${basePath || ''}${src}`
 
-  return <NextImage src={imageSrc} unoptimized={isExternal} {...rest} />
+  return <NextImage src={`${basePath || ''}${src}`} unoptimized={true} {...rest} />
 
 }
 
